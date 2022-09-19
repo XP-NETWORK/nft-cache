6b83126d5f11d42ccc0be84812a3792d0a4efe8e
AlexeyAdoniev <Alexeya@xp.network>
2022-09-19 17:18:24 +0300

    bump-parser

--- src/controllers/parseNft.ts
+++ src/controllers/parseNft.ts
@@ -5,7 +5,7 @@ export const parseNft = async (req: Request, res: Response) => {
   const { uri, native, collectionIdent } = req.body;
 
   if (!native?.chainId || !collectionIdent || !native?.tokenId || !uri) {
-    return res.send("key parameter missing");
+    return res.status(422).send("key parameter missing");
   }
 
   try {
@@ -19,8 +19,13 @@ export const parseNft = async (req: Request, res: Response) => {
       true
     );
 
+    if (parsed.errorStatus === 404) {
+      return res.status(404).json(parsed);
+    }
+
     res.status(200).json(parsed);
   } catch (e: any) {
-    res.status(500).end();
+    console.log(e, "e");
+    res.status(422).send(e.message);
   }
 };
