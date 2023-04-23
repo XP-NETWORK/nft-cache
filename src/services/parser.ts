60113ce035f41ca94198fce18cc1814612da1125
AlexeyAdoniev <Alexeya@xp.network>
2023-04-23 13:59:17 +0300

    bump parser

--- src/services/parser.ts
+++ src/services/parser.ts
@@ -6,34 +6,34 @@ import { config } from "dotenv";
 config();
 
 class Parser {
-  constructor() {
-    Moralis.start({
-      apiKey: process.env.MORALIS_KEY!,
-    });
-    injectMoralis(Moralis);
-  }
+    constructor() {
+        Moralis.start({
+            apiKey: process.env.MORALIS_KEY!,
+        });
+        injectMoralis(Moralis);
+    }
 
-  prepareNft(nft: parsedNft) {
-    const { metaData } = nft;
+    prepareNft(nft: parsedNft) {
+        const { metaData } = nft;
 
-    return {
-      ...nft,
-      uri:
-        metaData?.animation_url && !metaData.image
-          ? metaData?.animation_url
-          : metaData.image,
-    };
-  }
+        return {
+            ...nft,
+            uri:
+                metaData?.animation_url && !metaData.image
+                    ? metaData?.animation_url
+                    : metaData.image,
+        };
+    }
 
-  async parseNft(nft: any, account: string, whitelisted: any) {
-    const parsed = await nftGeneralParser(nft, account, whitelisted).catch(
-      (e) => {
-        throw e;
-      }
-    );
+    async parseNft(nft: any, account: string, whitelisted: any) {
+        const parsed = await nftGeneralParser(nft, account, whitelisted).catch(
+            (e) => {
+                throw e;
+            }
+        );
 
-    return this.prepareNft(parsed);
-  }
+        return this.prepareNft(parsed);
+    }
 }
 
 export default () => new Parser();
